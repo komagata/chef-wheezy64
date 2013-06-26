1d6509bb2073dd38f062ed925774f6afe7b3b345
Masaki Komagata <komagata@gmail.com>
2013-06-26 14:34:25 +0900

    Fix

--- site-cookbooks/hello/recipes/default.rb
+++ site-cookbooks/hello/recipes/default.rb
@@ -7,3 +7,12 @@
 # All rights reserved - Do Not Redistribute
 #
 log "Hello, knife solo!!"
+
+package "nginx" do
+  action :install
+end
+
+service "nginx" do
+  supports :status => true, :restart => true, :reload => true
+  action [:enable, :start]
+end
